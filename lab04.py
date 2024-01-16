ec99eccc2b3d6c1cd325f76a68d47bbc4f72d2f9
piyapong <piyapong.wongk@gmail.com>
2024-01-16 11:04:55 +0700

    first commit2

---
+++ lab04.py
@@ -0,0 +1,60 @@
+# Type of data ชนิดของข้อมูล
+# 1. Number ตัวเลข
+#    1.1 Integer (int) จำนวนเต็ม +, -, 0
+#    1.2 Float เลขจุดทศนิยม
+# 2. String (str) ตัวอักษร
+# 3. List
+# 4. Tuple
+# 5. Set
+# 6. Dictionary
+
+# Integer (int) จำนวนเต็ม +, -, 0
+a = 5
+b = -5
+c = 0
+
+print(a)
+print(b)
+print(c)
+
+# Check data type >>> type()
+print()
+print(type(a))
+print(type(b))
+print(type(c))
+print()
+
+print()
+print("a type: ", type(a))
+print("b type: ", type(b))
+print("c type: ", type(c))
+print()
+
+print()
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print()
+
+print()
+print("a+b type: ", type(a+b))
+print("a-b type: ", type(a-b))
+print("a*b type: ", type(a*b))
+print("a/b type: ", type(a/b))
+print()
+
+# String (str) ตัวอักษร
+Name = "Jack"
+Sename = "Saru"
+Age = 41
+GPA = 3.33
+Address = "56/2"
+
+print()
+print("Name type: ", type(Name))
+print("Sename type: ", type(Sename))
+print("Age type: ", type(Age))
+print("GPA type: ", type(GPA))
+print("Address type: ", type(Address))
+print()
