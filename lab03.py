2b49510fac5ca94e8771f8abd163abf8d96ee0cd
piyapong <piyapong.wongk@gmail.com>
2024-01-16 11:01:21 +0700

    first commit2

---
+++ lab03.py
@@ -0,0 +1,10 @@
+# data and variable 
+# ข้อมูลและตัวแปร
+Temperature = 30 # การสร้างตัวแปรและกำหนดข้อมูลที่เป็น "ตัวเลข"
+Humidity = 65
+Name = "Jack" # การสร้างตัวแปรและกำหนดข้อมูลที่เป็น "ตัวอักษร"
+Sername = "Hana"
+print(Temperature)
+print(Humidity)
+print(Name)
+print(Sername)
